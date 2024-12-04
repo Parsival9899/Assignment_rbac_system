cc99ca6e675efb2d0f88b5914e2af42a2cd75f6d
Jeetu Singh <ps588612@gmail.com>
2024-12-05 02:20:24 +0530

    Updates

---
+++ precache-manifest.e5d429250b8674eb5c249f73dc36d290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d5b560a7f15326f66b06c79f6bce32",
+    "url": "/Assignment_rbac_system/index.html"
+  },
+  {
+    "revision": "4cbac98d6478b7d4bcaa",
+    "url": "/Assignment_rbac_system/static/css/main.98bb087f.chunk.css"
+  },
+  {
+    "revision": "c27d8450ab8dfa455c44",
+    "url": "/Assignment_rbac_system/static/js/2.f841ee14.chunk.js"
+  },
+  {
+    "revision": "4cbac98d6478b7d4bcaa",
+    "url": "/Assignment_rbac_system/static/js/main.773d6344.chunk.js"
+  },
+  {
+    "revision": "a16d0415eaac38905f82",
+    "url": "/Assignment_rbac_system/static/js/runtime~main.572ee363.js"
+  }
+]);
